e4dfccd7ea49ff4b9a04f6647e93df7805818777
Oleksii Novikov <oleksii@novikraft.com>
2021-05-06 21:14:56 +0300

    chore: add shor link to the integer's stringifier and exception

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { Exception } from './exception';
 export { Stringifier } from './stringifier';
+
+export { IntStr } from './int-str/int-str';
+export { IntThousandExp } from './int-str/int-thousand-exp';
